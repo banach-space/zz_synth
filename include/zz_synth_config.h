16294c09422d5aecba2650dde67f817b421daad0
zimzum <zimzum@users.noreply.github.com>
2014-12-24 17:17:57 +0000

    Adding files. No meaningful content yet

---
+++ include/zz_synth_config.h
@@ -0,0 +1,19 @@
+/*=============================================================
+  FILE:
+    include/zz_synth_config.h
+
+  AUTHOR:
+    zimzum@github 
+
+  DESCRIPTION:
+    The definition of the zz_synth_config class. The configuration
+    class for the zz_synth library.
+ 
+  License: GNU GPL v2.0 
+  =============================================================*/
+
+#ifndef _SYNTH_CONFIG_H_
+#define _SYNTH_CONFIG_H_
+
+
+#endif /* _SYNTH_CONFIG_H_ */
