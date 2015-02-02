d954388d55470be30b402f62e3f573b876752f1c
zimzum <zimzum@users.noreply.github.com>
2015-02-02 21:23:43 +0000

    Began remodelling envelope generation. Added seperate class called Segment that's gonna be used for member Envelop's member variables. Work in progress.

--- include/common/envelope.h
+++ include/common/envelope.h
@@ -17,6 +17,96 @@
 #include <common/zz_global_include.h>
 
 //========================================================================
+// CLASS: Segment
+//
+// DESCRIPTION:
+//      A segment in an envelope. Abstract base class.
+//========================================================================
+class Segment
+{
+public:
+    //--------------------------------------------------------------------
+    // 1. CONSTRUCTORS/DESTRUCTOR/ASSIGNMENT OPERATORS
+    //--------------------------------------------------------------------
+    explicit Segment(float peak_amplitude_arg, size_t number_of_steps_arg);
+    virtual ~Segment() = 0;
+    explicit Segment(const Segment& rhs) = delete;
+    explicit Segment(Segment&& rhs) = delete;
+    Segment& operator=(const Segment &rhs) = delete;
+    Segment& operator=(Segment&& rhs) = delete;
+
+    //--------------------------------------------------------------------
+    // 2. GENERAL USER INTERFACE 
+    //--------------------------------------------------------------------
+    const vector<float> GetSegment();
+    bool Init(size_t number_of_steps_arg);
+
+    //--------------------------------------------------------------------
+    // 3. ACCESSORS
+    //--------------------------------------------------------------------
+    // None
+
+private:
+    //--------------------------------------------------------------------
+    // 4. PRIVATE METHODS 
+    //--------------------------------------------------------------------
+    virtual const vector<float> GenerateSegment(
+        size_t number_of_samples,
+        float peak_amplitude) = 0;
+
+    //--------------------------------------------------------------------
+    // 5. DATA MEMMBERS 
+    //--------------------------------------------------------------------
+    size_t number_of_steps_;
+    vector<float> segment_;
+    float peak_amplitude_;
+    // The init_ variable is set by default to 'false' and then changed to
+    // 'true' once the segment_ vector is set. This is done by the Init()
+    // function.
+    bool init_;
+};
+
+//========================================================================
+// CLASS: LinearSegment
+//
+// DESCRIPTION:
+//      A linear segment in an envelope: y = a*x + b
+//========================================================================
+class LinearSegment : public Segment
+{
+public:
+    //--------------------------------------------------------------------
+    // 1. CONSTRUCTORS/DESTRUCTOR/ASSIGNMENT OPERATORS
+    //--------------------------------------------------------------------
+    explicit LinearSegment(float peak_amplitude_arg, size_t number_of_steps_arg);
+    ~LinearSegment();
+
+    //--------------------------------------------------------------------
+    // 2. GENERAL USER INTERFACE 
+    //--------------------------------------------------------------------
+
+    //--------------------------------------------------------------------
+    // 3. ACCESSORS
+    //--------------------------------------------------------------------
+    // None
+
+private:
+    //--------------------------------------------------------------------
+    // 4. PRIVATE METHODS 
+    //--------------------------------------------------------------------
+    const vector<float> GenerateSegment(
+        size_t number_of_samples,
+        float peak_amplitude);
+
+    //--------------------------------------------------------------------
+    // 5. DATA MEMMBERS 
+    //--------------------------------------------------------------------
+    size_t number_of_steps_;
+    vector<float> segment_;
+    float peak_amplitude_;
+};
+
+//========================================================================
 // CLASS: Envelope
 //
 // DESCRIPTION:
@@ -30,12 +120,7 @@ public:
     //--------------------------------------------------------------------
     // Make this class pure abstract and block copy constructor, assignment
     // operator and the corresponding move operators.
-    explicit Envelope(
-            SynthConfig& synthesiser,
-            int32_t peak_amplitude_arg,
-            uint32_t attack_duration_arg,
-            uint32_t decay_duration_arg
-            );
+    explicit Envelope();
     virtual ~Envelope() = 0;
     explicit Envelope(const Envelope& rhs) = delete;
     explicit Envelope(Envelope&& rhs) = delete;
@@ -45,15 +130,12 @@ public:
     //--------------------------------------------------------------------
     // 2. GENERAL USER INTERFACE 
     //--------------------------------------------------------------------
-    virtual vector<float> GenerateAttack() const;
-    virtual vector<float> GenerateDecay() const;
+    // None
 
     //--------------------------------------------------------------------
     // 3. ACCESSORS
     //--------------------------------------------------------------------
-    float peak_amplitude() const;
-    size_t attack_number_of_samples() const;
-    size_t decay_number_of_samples() const;
+    // None
 
 protected:
     //--------------------------------------------------------------------
@@ -65,35 +147,30 @@ private:
     //--------------------------------------------------------------------
     // 5. DATA MEMMBERS 
     //--------------------------------------------------------------------
-    int32_t peak_amplitude_;
-    uint32_t attack_duration_;
-    uint32_t decay_duration_;
-    size_t attack_number_of_samples_; 
-    size_t decay_number_of_samples_; 
+    //
     
 };
 
 //========================================================================
-// CLASS: LinearEnvelope
+// CLASS: ArEnvelope
 //
 // DESCRIPTION:
-//      Linear envelope (linear attack and decay).
+//      AR envelope (attack/sustain/release) with the sustain level set
+//      automatically to 100%.
 //========================================================================
-class LinearEnvelope : public Envelope
+class ArEnvelope : public Envelope
 {
 public:
     //--------------------------------------------------------------------
     // 1. CONSTRUCTORS/DESTRUCTOR/ASSIGNMENT OPERATORS
     //--------------------------------------------------------------------
-    // Make this class pure abstract and block copy constructor, assignment
-    // operator and the corresponding move operators.
-    explicit LinearEnvelope(
+    explicit ArEnvelope(
             SynthConfig& synthesiser,
             int32_t peak_amplitude_arg,
             uint32_t attack_duration_arg,
             uint32_t decay_duration_arg
             );
-    virtual ~LinearEnvelope() = default;
+    virtual ~ArEnvelope() = default;
 
     //--------------------------------------------------------------------
     // 2. GENERAL USER INTERFACE 
@@ -116,9 +193,12 @@ private:
     //--------------------------------------------------------------------
     // 5. DATA MEMMBERS 
     //--------------------------------------------------------------------
-    float attack_increment_;
-    float decay_decrement_;
+    size_t attack_number_of_samples_;
+    size_t decay_number_of_samples_;
+    LinearSegment decay_segment_;
+    LinearSegment attack_segment_;
     
 };
+
 
 #endif /* #define _ENVELOPE_H_ */
--- src/common/envelope.cc
+++ src/common/envelope.cc
@@ -14,6 +14,82 @@
 #include <common/envelope.h>
 
 //========================================================================
+// CLASS: Segment
+//========================================================================
+//------------------------------------------------------------------------
+// 1. CONSTRUCTORS/DESTRUCTOR/ASSIGNMENT OPERATORS
+//------------------------------------------------------------------------
+Segment::Segment(float peak_amplitude_arg, size_t number_of_steps_arg) :
+    peak_amplitude_(peak_amplitude_arg),
+    number_of_steps_(number_of_steps_arg),
+    init_(false)
+{}
+
+Segment::~Segment() {};
+
+//------------------------------------------------------------------------
+// 2. GENERAL USER INTERFACE 
+//------------------------------------------------------------------------
+bool Segment::Init(size_t number_of_samples)
+{
+
+}
+const vector<float> Segment::GetSegment()
+{
+    if (!init_)
+    {
+        segment_ = GenerateSegment(number_of_steps_, peak_amplitude_);
+        init_ = true;
+    }
+
+    return segment_; 
+}
+
+//========================================================================
+// CLASS: LinearSegment
+//========================================================================
+//------------------------------------------------------------------------
+// 1. CONSTRUCTORS/DESTRUCTOR/ASSIGNMENT OPERATORS
+//------------------------------------------------------------------------
+LinearSegment::LinearSegment(
+        float peak_amplitude_arg, 
+        size_t number_of_steps_arg) :
+    Segment(peak_amplitude_arg, number_of_steps_arg)
+{}
+
+LinearSegment::~LinearSegment() 
+{}
+
+//------------------------------------------------------------------------
+// 2. GENERAL USER INTERFACE 
+//------------------------------------------------------------------------
+const vector<float> LinearSegment::GenerateSegment(
+        size_t number_of_samples,
+        float peak_amplitude)
+{
+    vector<float> amplitudes;
+    amplitudes.reserve(number_of_samples);
+    float volume = 0;
+    float increment = peak_amplitude / number_of_samples;
+    vector<float>::iterator it;
+
+    for (it = amplitudes.begin();
+            it != amplitudes.end();
+            it++)
+    {
+        *it = volume;
+        volume += increment;
+    }
+
+    // Due to rounding error the last entry might be different from 
+    // peak_amplitude. Force it to be equal.
+    *it = peak_amplitude;
+
+    return amplitudes;
+
+}
+
+//========================================================================
 // CLASS: Envelope
 //========================================================================
 //------------------------------------------------------------------------
@@ -29,7 +105,9 @@ Envelope::Envelope(
     attack_duration_(attack_duration_arg),
     decay_duration_(decay_duration_arg),
     attack_number_of_samples_(0),
-    decay_number_of_samples_(0)
+    decay_number_of_samples_(0),
+    attack_(0),
+    decay_(0)
 {
 
     attack_number_of_samples_ = attack_duration_ * synthesiser.sampling_rate();
@@ -58,25 +136,28 @@ size_t Envelope::decay_number_of_samples() const{return decay_number_of_samples_
 
 
 //========================================================================
-// CLASS: LinearEnvelope
+// CLASS: ArEnvelope
 //========================================================================
 //------------------------------------------------------------------------
 // 1. CONSTRUCTORS/DESTRUCTOR/ASSIGNMENT OPERATORS
 //------------------------------------------------------------------------
-LinearEnvelope::LinearEnvelope(
+ArEnvelope::ArEnvelope(
         SynthConfig& synthesiser,
         int32_t peak_amplitude_arg,
         uint32_t attack_duration_arg,
         uint32_t decay_duration_arg
         ) : 
-    Envelope(
-            synthesiser,
-            peak_amplitude_arg, 
-            attack_duration_arg,
-            decay_duration_arg)
+    Envelope(),
+    decay_segment_(peak_amplitude_arg, 0),
+    attack_segment_(peak_amplitude_arg, 0)
 {
-    attack_increment_ = peak_amplitude() / attack_number_of_samples();
-    decay_decrement_ = peak_amplitude() / decay_number_of_samples();
+    size_t decay_number_of_samples = decay_duration_arg 
+        * synthesiser.sampling.rate();
+    size_t attack_number_of_samples = attack_duration_arg 
+        * synthesiser.sampling.rate();
+    
+    decay_segment_.Init(decay_number_of_samples);
+    attack_segment_.Init(attack_number_of_samples);
 }
 
 //------------------------------------------------------------------------
@@ -136,3 +217,4 @@ vector<float> LinearEnvelope::GenerateDecay() const
 // 4. MUTATORS
 //------------------------------------------------------------------------
 // None
+
