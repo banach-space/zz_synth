4a264fc4604194e3a593c2ccc904d2735cb3a28e
zimzum <zimzum@users.noreply.github.com>
2015-02-16 21:22:05 +0000

    Added test for checking different decay semgnet's lenght. Crashing, so commented it out. Will debug tomorrow.

--- unit_tests/envelope/testbench.cc
+++ unit_tests/envelope/testbench.cc
@@ -46,9 +46,9 @@ static void ComparePreAndPostEnvelopeBasic(
 //      ComparePreAndPostEnvelopeBasic
 //  
 //  DESCRIPTION:
-//      By comparing the pre- and post- envelope samples verifies that
-//      conditions that have to be satisfied for every AR envelope are
-//      indeed met.
+//      By comparing the pre- and post- envelope samples, this function
+//      verifies that conditions that have to be satisfied for an AR
+//      envelope are indeed met.
 //  INPUT:
 //      attack_length         - the length of the attack segment
 //      decay_length          - the length of the decay segment
@@ -58,28 +58,6 @@ static void ComparePreAndPostEnvelopeBasic(
 //      None
 //------------------------------------------------------------------------
 static void ComparePreAndPostEnvelopeBasic(
-        vector<int16_t>::difference_type attack_length, 
-        vector<int16_t>::difference_type decay_length,
-        vector<int16_t> &samples_pre_envelope,
-        vector<int16_t> &samples_post_envelope);
-//------------------------------------------------------------------------
-//  NAME:
-//      ComparePreAndPostEnvelopeNonZeroAttack
-//  
-//  DESCRIPTION:
-//      By comparing the pre- and post- envelope samples verifies that
-//      conditions that have to be satisfied for an AR envelope with
-//      an attack segment longer than zero samples are indeed met.
-//  INPUT:
-//      attack_length         - the length of the attack segment (has to
-//                              be greater than 0)
-//      decay_length          - the length of the decay segment
-//      samples_pre_envelope  - samples before applying the envelope
-//      samples_post_envelope - samples after applying the envelope
-//  OUTPUT:
-//      None
-//------------------------------------------------------------------------
-static void ComparePreAndPostEnvelopeNonZeroAttack(
         vector<int16_t>::difference_type attack_length, 
         vector<int16_t>::difference_type decay_length,
         vector<int16_t> &samples_pre_envelope,
@@ -109,14 +87,61 @@ static void ComparePreAndPostEnvelopeBasic(
             ::testing::ContainerEq(sustain_segment_without_envelope));
 
     // 5. The first and the last element of 'enveloped' samples should be identical and equal to 0.
-    EXPECT_EQ(samples_post_envelope.front(), samples_post_envelope.back());
-    EXPECT_EQ(samples_post_envelope.front(), 0);
+    //
+    // NOTE: This test makes only sense for AR envelopes with the attack and decay segments 
+    //        longer than 0 samples
+    if ((decay_length > 0) && (attack_length > 0))
+    {
+        EXPECT_EQ(samples_post_envelope.front(), samples_post_envelope.back());
+        EXPECT_EQ(samples_post_envelope.front(), 0);
+    }
 
     // 6. The sample at the end of attack should be equal to the original one
     EXPECT_EQ(*(samples_post_envelope.begin() + attack_length), *(samples_pre_envelope.begin() + attack_length));
 
     // 7. The element at the beginning of decay should be equal to the original one
     EXPECT_EQ(*(samples_post_envelope.end() - decay_length), *(samples_pre_envelope.end() - decay_length));
+
+    // 8. The second element in the attack segment should be different from the original one. Note that 
+    // special care have to be taken if the original sample was equal to 0. To be more precise, if 
+    // the 2nd element in the attack segment (original samples), (samples_pre_envelope.begin() + 1), is equal
+    // to 0 then we have to take (samples_post_envelope.begin() + 2) instead. If the second pick is also 0
+    // then assume that the whole vector is filled with zeros and there's no point asserting for 'not equal'.
+    //
+    // NOTE: This test makes only sense for AR envelopes with the attack segment longer than 0 samples
+    if (attack_length > 0)
+    {
+        if (*(samples_pre_envelope.begin() + 1) != 0)
+        {
+            EXPECT_NE(*(samples_post_envelope.begin() + 1), *(samples_pre_envelope.begin() + 1));
+        }else if(*(samples_pre_envelope.begin() + 2) != 0 )
+        {
+            EXPECT_NE(*(samples_post_envelope.begin() + 2), *(samples_pre_envelope.begin() + 2));
+        }
+    }
+
+    // 9. The second element in the decay segment should be different from the original one. Note that 
+    // special care have to be taken if the original sample was equal to 0. To be more precise, if 
+    // the 2nd element in the attack segment (original samples), (samples_pre_envelope.end() - decay_length + 1),
+    // is equal to 0 then we have to take (samples_post_envelope.end() - decay_length + 2) instead. If the
+    // second pick is also 0 then assume that the whole vector is filled with zeros and there's no point
+    // asserting for 'not equal'.
+    //
+    // NOTE: This test makes only sense for AR envelopes with the decay segment longer than 0 samples
+    if (decay_length > 0)
+    {
+        if (*(samples_pre_envelope.end() - decay_length + 1) != 0)
+        {
+            EXPECT_NE(
+                    *(samples_post_envelope.end() - decay_length + 1), 
+                    *(samples_pre_envelope.end() - decay_length + 1));
+        }else if(*(samples_pre_envelope.end() - decay_length + 2) != 0 )
+        {
+            EXPECT_NE(
+                    *(samples_post_envelope.end() - decay_length + 2), 
+                    *(samples_pre_envelope.end() - decay_length + 2));
+        }
+    }
 
 }
 
@@ -140,36 +165,6 @@ static void ComparePreAndPostEnvelopeNonZeroAttack(
     sustain_segment_end   = samples_post_envelope.end() - decay_length;
     vector<int16_t> sustain_segment_with_envelope(sustain_segment_begin, sustain_segment_end);
 
-    // 4. The second element in the attack segment should be different from the original one. Note that 
-    // special care have to be taken if the original sample was equal to 0. To be more precise, if 
-    // the 2nd element in the attack segment (original samples), (samples_pre_envelope.begin() + 1), is equal
-    // to 0 then we have to take (samples_post_envelope.begin() + 2) instead. If the second pick is also 0
-    // then assume that the whole vector is filled with zeros and there's no point asserting for 'not equal'.
-    if (*(samples_pre_envelope.begin() + 1) != 0)
-    {
-        EXPECT_NE(*(samples_post_envelope.begin() + 1), *(samples_pre_envelope.begin() + 1));
-    }else if(*(samples_pre_envelope.begin() + 2) != 0 )
-    {
-        EXPECT_NE(*(samples_post_envelope.begin() + 2), *(samples_pre_envelope.begin() + 2));
-    }
-
-    // 5. The second element in the decay segment should be different from the original one. Note that 
-    // special care have to be taken if the original sample was equal to 0. To be more precise, if 
-    // the 2nd element in the attack segment (original samples), (samples_pre_envelope.end() - decay_length + 1),
-    // is equal to 0 then we have to take (samples_post_envelope.end() - decay_length + 2) instead. If the
-    // second pick is also 0 then assume that the whole vector is filled with zeros and there's no point
-    // asserting for 'not equal'.
-    if (*(samples_pre_envelope.end() - decay_length + 1) != 0)
-    {
-        EXPECT_NE(
-                *(samples_post_envelope.end() - decay_length + 1), 
-                *(samples_pre_envelope.end() - decay_length + 1));
-    }else if(*(samples_pre_envelope.end() - decay_length + 2) != 0 )
-    {
-        EXPECT_NE(
-                *(samples_post_envelope.end() - decay_length + 2), 
-                *(samples_pre_envelope.end() - decay_length + 2));
-    }
 
 }
 
@@ -269,7 +264,7 @@ TEST(ArEnvelopeGenerationTest, HandleDifferentOveralDuration)
 {
     size_t pitch              = kNumberOfFrequencies/size_t(2);
     int32_t volume            = 1 << 7;
-    vector<uint32_t> duration = {0, 5, 10};
+    vector<uint32_t> duration = {2, 5, 10};
     double initial_phase      = 0;
     double peak_amplitude     = 1;
     double decay_duration     = 1;
@@ -315,6 +310,60 @@ TEST(ArEnvelopeGenerationTest, HandleDifferentOveralDuration)
                 samples_post_envelope);
     }
 }
+
+//TEST(ArEnvelopeGenerationTest, HandleDifferentDecaykDuration)
+//{
+    //size_t pitch                   = kNumberOfFrequencies/size_t(2);
+    //int32_t volume                 = 1 << 7;
+    //uint32_t duration              = 5;
+    //double initial_phase           = 0;
+    //double peak_amplitude          = 1;
+    //double attack_duration          = 1;
+    //vector<double> decay_duration = {0, 1, 2.3, 3, 4};
+    //const string file_name("test_pitch.wav");
+
+    //// Initialise the synthesiser
+    //SynthConfig &synthesiser  = SynthConfig::getInstance();
+    //synthesiser.Init();	
+
+
+    //for (vector<double>::iterator it = decay_duration.begin(), it_end = decay_duration.end(); 
+            //it != it_end; 
+            //it++)
+    //{
+        //// 1. Initialise the envelope
+        //ArEnvelope envelope(synthesiser, peak_amplitude, attack_duration, *it);
+
+        //// The length of attack and decay
+        //vector<int16_t>::difference_type attack_length = 
+            //static_cast<vector<int16_t>::difference_type>(attack_duration * synthesiser.sampling_rate());
+        //vector<int16_t>::difference_type decay_length  = 
+            //static_cast<vector<int16_t>::difference_type>(*it * synthesiser.sampling_rate());
+
+        //// 2. Generate the samples and apply the envelope
+        //Oscillator osc(synthesiser, volume , initial_phase, pitch);
+        //vector<int16_t> samples_pre_envelope  = osc(duration);
+
+        //// 3. Create a copy of the generated samples and apply the envelope
+        //vector<int16_t> samples_post_envelope  = samples_pre_envelope;
+        //envelope.ApplyEnvelope(samples_post_envelope);
+
+        //// 4. Validate the application of the envelope by comapring the pre- 
+        //// and post- enevelope samples
+        //if (
+                //(samples_pre_envelope.begin() == samples_pre_envelope.end()) 
+                //&& (samples_post_envelope.begin() == samples_post_envelope.end())
+                //&& (*it == 0))
+        //{
+            //continue;
+        //}
+        //ComparePreAndPostEnvelopeBasic(
+                //attack_length, 
+                //decay_length,
+                //samples_pre_envelope,
+                //samples_post_envelope);
+    //}
+//}
 
 TEST(ArEnvelopeGenerationTest, HandleDifferentAttackDuration)
 {
@@ -369,6 +418,7 @@ TEST(ArEnvelopeGenerationTest, HandleDifferentAttackDuration)
                 samples_post_envelope);
     }
 }
+
 
 //========================================================================
 // MAIN 
